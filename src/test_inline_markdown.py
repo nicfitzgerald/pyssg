0ece3483d7340229b53604c8b60afb13e10cba74
Nic FitzGerald <nicholas.a.fitzgerald@gmail.com>
2025-03-15 10:17:30 -0500

    Add link and image extraction

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,5 +1,9 @@
 import unittest
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import (
+  split_nodes_delimiter,
+  extract_markdown_images,
+  extract_markdown_links,
+)
 from textnode import TextNode, TextType
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -81,6 +85,28 @@ class TestInlineMarkdown(unittest.TestCase):
       ],
       new_nodes,
     )
+
+  def text_extract_markdown_images(self):
+    matches = extract_markdown_images(
+      "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+    )
+    self.assertListEqual(
+      [("image", "https://i.imgur.com/zjjcJKZ.png")],
+      matches
+    )
+
+  def text_extract_markdown_links(self):
+    matches = extract_markdown_links(
+      "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+    )
+    self.assertListEqual(
+      [
+        ("link", "https://boot.dev"),
+        ("another link", "https://blog.boot.dev"),
+       ],
+      matches
+    )
+
 
 if __name__ == "__main__":
   unittest.main()
